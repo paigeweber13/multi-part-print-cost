7a093848a7afa83c11722288f3b83fa22c2521e6
Riley Weber <rileyw13@protonmail.com>
2019-05-01 13:46:09 -0400

    moved model file location

--- test/test-slice-single-model.sh
+++ test/test-slice-single-model.sh
@@ -3,6 +3,6 @@
 export CURA_ENGINE_SEARCH_PATH=/resources/definitions:/resources/extruders
 
 SETTINGS_JSON_TO_USE=../bin/resources/definitions/fdmprinter.def.json
-MODEL_TO_SLICE=bulbasaur.stl
+MODEL_TO_SLICE=models/bulbasaur.stl
 
 ../bin/CuraEngine slice -j $SETTINGS_JSON_TO_USE -l $MODEL_TO_SLICE
