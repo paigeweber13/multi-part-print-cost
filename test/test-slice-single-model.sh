8a597d35b0bbe3968b32ec59a00ff26b1a38ef98
Riley Weber <rileyw13@protonmail.com>
2019-04-30 21:37:13 -0400

    ignores output gcode files

--- test/test-slice-single-model.sh
+++ test/test-slice-single-model.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export CURA_ENGINE_SEARCH_PATH=/resources/definitions:/resources/extruders
-SETTINGS_TO_USE=../bin/resources/definitions/fdmprinter.def.json
+
+SETTINGS_JSON_TO_USE=../bin/resources/definitions/fdmprinter.def.json
 MODEL_TO_SLICE=bulbasaur.stl
 
-../bin/CuraEngine slice -j $SETTINGS_TO_USE -l $MODEL_TO_SLICE
+../bin/CuraEngine slice -j $SETTINGS_JSON_TO_USE -l $MODEL_TO_SLICE
