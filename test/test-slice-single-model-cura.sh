55f479b57785b443b48af599eb35edee2ea94c67
Riley Weber <rileyw13@protonmail.com>
2019-05-03 14:22:13 -0400

    removed unused stuff

--- test/test-slice-single-model-cura.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CURA_ENGINE_SEARCH_PATH=/resources/definitions:/resources/extruders
-
-SETTINGS_JSON_TO_USE=../bin/resources/definitions/fdmprinter.def.json
-MODEL_TO_SLICE=models/bulbasaur.stl
-
-../bin/CuraEngine slice -j $SETTINGS_JSON_TO_USE -l $MODEL_TO_SLICE
