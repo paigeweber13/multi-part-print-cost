c5c053712a81043029b36ef941eaf7ebdc9f0979
Riley Weber <rileyw13@protonmail.com>
2019-05-05 18:25:22 -0400

    added docstrings

--- test/test_core_functions.py
+++ test/test_core_functions.py
@@ -1,13 +1,21 @@
+"""
+tests core functionality. assumes that you are running tests from the root of
+the project directory
+"""
+
 import os
 import unittest
 
 import multipartprintpy.core as mpp
 
-"""
-assumes that you are running tests from the root of the project directory
-"""
 class TestCoreFunctions(unittest.TestCase):
+    """
+    tests things like slicing models and parsing the time/filament usage data
+    """
     def test_slice_single_model(self):
+        """
+        tries to slice a single model with 0.2mm layer height and checks to see if the file exists. Not a really robust check, but it works
+        """
         try:
             os.remove('test/models/bulbasaur-0.2mm.gcode')
         except FileNotFoundError:
@@ -19,16 +27,20 @@ class TestCoreFunctions(unittest.TestCase):
             '--layer-height', '0.2', 'test/models/bulbasaur.stl',
             '--output', 'test/models/bulbasaur-0.2mm.gcode'
             ]
-        self.assertEqual(expected_command, mpp.slice_model(0.2, 0, 'test/'
-            + 'models/bulbasaur.stl'))
+        self.assertEqual(expected_command,
+                         mpp.slice_model(0.2, 0, 'test/' +
+                                         'models/bulbasaur.stl'))
         self.assertTrue(os.path.isfile('test/models/bulbasaur-0.2mm.gcode'))
 
     def test_scrape_time_and_usage_estimates_from_gcode(self):
-        expected_result = 'filament used: 4.01m\n' \
-                        + '               9.6cm3\n' \
-                        + '               12.0g\n' \
-                        + 'filament cost: 0.2 USD\n' \
-                        + 'estimated print time: 52m28s\n'
+        """
+        sees if scrape_time function can get the data from a gcode
+        """
+        expected_result = 'filament used: 4.01m\n' +\
+                          '               9.6cm3\n' +\
+                          '               12.0g\n' +\
+                          'filament cost: 0.2 USD\n' +\
+                          'estimated print time: 52m28s\n'
         actual_result = mpp.scrape_time_and_usage_estimates(
             ['bulbasaur-0.2mm.gcode'])
         self.assertEqual(expected_result, actual_result)
