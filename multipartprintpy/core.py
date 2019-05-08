0262294090e86599abd259f00a6ffa40094495f9
Riley Weber <rileyw13@protonmail.com>
2019-05-08 14:38:05 -0400

    estimating time now uses timedelta

--- multipartprintpy/core.py
+++ multipartprintpy/core.py
@@ -43,25 +43,48 @@ def scrape_time_and_usage_estimates(list_of_files: typing.List[str]):
         ;\ filament\ used\ =\ (?P<g_usage>\d+\.\d+)\n
         ;\ filament\ cost\ =\ (?P<usd_cost>\d+\.\d+)\n
         .*\n
-        ;\ estimated\ printing\ time\ \(normal\ mode\)\ =\ (?P<time>(\d+h
-        \ )?(\d+m\ )?\d+s)$
+        ;\ estimated\ printing\ time\ \(normal\ mode\)\ =
+        \ (?P<time> (\d+d\ )? (\d+h\ )? (\d+m\ )? \d+s) $
         """, re.VERBOSE | re.MULTILINE)
     for gcode_file in list_of_files:
         my_match = None
+        # print_time[0] is days, print_time[1] is hours, index 2 is minutes, 
+        # and index 3 is seconds
+        time_matches = []
+        print_time = [0,0,0,0]
+
         try:
             with open(gcode_file, 'r') as open_gcode:
                 my_match = estimate_regex.search(open_gcode.read())
+                if my_match is None:
+                    raise SyntaxError
+                print_time_string = my_match.group('time')
+                # print(print_time_string)
+                time_matches.append(re.search(r'(\d+)d', print_time_string))
+                time_matches.append(re.search(r'(\d+)h', print_time_string))
+                time_matches.append(re.search(r'(\d+)m', print_time_string))
+                time_matches.append(re.search(r'(\d+)s', print_time_string))
+                # print(time_matches)
+                for i in range(4):
+                    if time_matches[i] is not None:
+                        print_time[i] = int(time_matches[i].group(1))
         except FileNotFoundError:
             print('file ' + gcode_file + ' not found, skipping...')
             continue
+        except SyntaxError:
+            print('file ' + gcode_file + ' does not have properly formatted'
+                + 'filament usage and time data. Skipping.')
 
         print(my_match)
 
         filament_usage_m = round(float(my_match.group(
             'mm_usage')) / 1000, 2)
-        print_time = datetime.datetime.strptime(my_match.group('time'),
+        # print_time = datetime.datetime.strptime(my_match.group('time'),
                                                 # '%Hh %Mm %Ss').time()
-                                                '%Mm %Ss').time()
+                                                # '%Mm %Ss').time()
+        print_time = datetime.timedelta(days=print_time[0],
+            hours=print_time[1], minutes=print_time[2],
+            seconds=print_time[3])
         result.append({
             'name-of-file': gcode_file,
             'filament-used-m': filament_usage_m,
