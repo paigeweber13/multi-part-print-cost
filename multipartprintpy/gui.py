018db01a2b6a5c67f01ffa589e4a981613f461e5
Riley Weber <rileyw13@protonmail.com>
2019-05-25 20:21:28 -0400

    created gui layout

--- multipartprintpy/gui.py
+++ multipartprintpy/gui.py
@@ -1,17 +1,31 @@
 import PySimpleGUI as sg
 import sys
 
+import multipartprintpy.core as mpp
+
 def main():
-    layout = [[sg.Text('Your typed chars appear here:'),
-               sg.Text('', key='_OUTPUT_') ],  
-              [sg.Input(do_not_clear=True, key='_IN_')],  
-              [sg.Text('Broswe for .stl files'), 
-               sg.InputText(key='_STL_FILES_'), sg.FilesBrowse()],
-              [sg.Button('Show'), sg.Button('Exit')]]  
+    layout = [
+            #   [sg.Text('Your typed chars appear here:'),
+            #    sg.Text('', key='_OUTPUT_') ],  
+            #   [sg.Input(do_not_clear=True, key='_IN_')],  
+            #   [sg.Button('Show'), sg.Button('Exit')],
+                 [sg.Text('Broswe for .stl files (Hold shift to select '\
+                          + 'multiple)')],
+                 [sg.InputText(key='_STL_FILES_'), sg.FilesBrowse()],
+                 [sg.Text('Select where the output data should be stored')],
+                 [sg.InputText(key='_OUTPUT_FILE_DIR_'), sg.FolderBrowse()],
+                 [sg.Text('Layer height')],
+                 [sg.Slider(range=(0.05, 0.35), default_value=(0.2), 
+                     resolution=(0.05), orientation='horizontal',
+                     key='_LAYER_HEIGHT_')],
+                 [sg.Checkbox('Generate supports', default=False,
+                     key='_GENERATE_SUPPORTS?_')],
+                 [sg.Button('Get Estimates')],
+             ]
 
     window = sg.Window('Multi Part Print Calculator', layout)  
 
-    while True:                 # Event Loop
+    while True: # Event Loop
       event, values = window.Read()  
       print(event, values)
       if event is None or event == 'Exit':  
