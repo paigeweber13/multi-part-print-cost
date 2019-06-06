341c4a1d2b6aa79c2c5a9b3cc15cfaac55105b87
Riley Weber <rileyw13@protonmail.com>
2019-06-06 13:42:51 -0400

    added comments in bash representing what I would like to see in an automatic builder for windows

---
+++ build-files/build-windows.bat
@@ -0,0 +1,7 @@
+REM cd ..
+REM export PYTHONPATH=$PYTHONPATH:$(pwd)
+REM pyinstaller build-files/multi-part-print-cost.spec
+REM cp README.md dist/
+REM cp LICENSE dist/
+REM cp bin dist/
+REM cp profiles dist/
