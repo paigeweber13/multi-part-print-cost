d49a02f5c42bc4b05f427242969cefcd9c547219
Riley Weber <rileyw13@protonmail.com>
2019-06-06 14:19:47 -0400

    added more notes on plan for windows build script

--- build-files/build-windows.bat
+++ build-files/build-windows.bat
@@ -5,3 +5,9 @@ REM cp README.md dist/
 REM cp LICENSE dist/
 REM cp bin dist/
 REM cp profiles dist/
+
+REM What would be done on ci?
+REM  - Unit tests
+REM  - build to pypi
+REM so basically there is no point except maybe to automate deployment to pypi
+REM then why not just use this script alone? And add separate build scripts for linux and mac?
