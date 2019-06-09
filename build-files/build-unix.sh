9d6ac3116735d066a67b7d11141ab178c1d2dd52
Riley Weber <rileyw13@protonmail.com>
2019-06-09 16:21:50 -0400

    renamed to linux

--- build-files/build-unix.sh
+++ build-files/build-unix.sh
@@ -2,7 +2,7 @@
 cd ..
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 rm -r dist
-pyinstaller build-files/multi-part-print-cost-unix.spec
+pyinstaller build-files/multi-part-print-cost-linux.spec
 chmod +x dist/multi-part-print-cost
 cp README.md dist/
 cp LICENSE dist/
@@ -11,7 +11,7 @@ cp -r profiles dist/
 
 version_filename="version"
 read VERSION < $version_filename
-output_filename="multi-part-print-cost-$VERSION-unix"
+output_filename="multi-part-print-cost-$VERSION-linux"
 rm -r $output_filename
 mv "dist" $output_filename
 tar -czf $output_filename.tgz $output_filename
