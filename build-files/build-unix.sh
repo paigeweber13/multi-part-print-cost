eaf8204c14fdee62d6c89d2b00017cb60d807d07
Riley Weber <rileyw13@protonmail.com>
2019-06-09 12:18:28 -0400

    made a couple fixes, added unix specific build file

--- build-files/build-unix.sh
+++ build-files/build-unix.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd ..
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-pyinstaller build-files/multi-part-print-cost.spec
+pyinstaller build-files/multi-part-print-cost-unix.spec
+chmod +x dist/multi-part-print-cost
 cp README.md dist/
 cp LICENSE dist/
-cp bin dist/
-cp profiles dist/
+cp -r bin dist/
+cp -r profiles dist/
